1835ce3763bb770f2f972c197ea930b91113bc9e
Somesh <somesh_nj2_076@masai.school>
2021-07-20 14:54:42 +0530

    home page navbar done

--- src/components/Home Page/NavBar.jsx
+++ src/components/Home Page/NavBar.jsx
@@ -1,23 +1,23 @@
 import React from "react"
-
+import styles from "../../styles/NavBar.module.css"
 
 export function NavBar() {
-    return <nav>
-            <div>
-            <img src={process.env.PUBLIC_URL+ "/NavBar_Logo.png"} alt="Logo"/>
-                <p>Search Jobs</p>
-                <p>Services</p>
-                <p>Resources</p>
-                <p>Recruiter</p>
-                <p>Walk-ins</p>
-                <p>Career Guidance</p>
+    return <nav className={styles.nav}>
+            <div className={styles.nav_left}>
+            <img src={process.env.PUBLIC_URL + "/NavBar_Logo.png"} alt="Logo" className={styles.logo}/>
+                <li className={styles.nav_tab}>Search Jobs</li>
+                <li className={styles.nav_tab}>Services</li>
+                <li className={styles.nav_tab}>Resources</li>
+                <li className={styles.nav_tab}>Recruiter</li>
+                <li className={styles.nav_tab}>Walk-ins</li>
+            <li className={styles.career_guidance}><img src={process.env.PUBLIC_URL + "/Career_bulb.png"  } alt="career"/>Career Guidance</li>
             </div>
-            <div>
-                <p>Create job alert</p>
-                <p>Post a job</p>
-                <div>
-                    <p>080-471055555</p>
-                    <p>9am-6pm, Mon to Sat</p>
+            <div className={styles.nav_right}>
+                <li>Create job alert</li>
+                <li>Post a job</li>
+                <div className={styles.nav_right_contact}>
+                    <li>080-471055555</li>
+                    <li>9am-6pm, Mon to Sat</li>
                 </div>
             </div>
     </nav>
