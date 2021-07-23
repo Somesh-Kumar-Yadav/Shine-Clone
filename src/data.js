72db0c6ebf14e49c9bd590993a2cb1a8624d6193
dhruva-surya <dhruva_nj2_108@masai.school>
2021-07-23 12:15:30 +0530

    data file is added

---
+++ src/data.js
@@ -0,0 +1,102 @@
+const data=[{
+    id:1,
+    jobTitle:"Client Support Analyst",
+    experience:1, //in years values[from 1 to 5]
+    salary:1.0, //laks per year value[from 1 to 5.5]
+    location:"Delhi", //first letter capital
+    keySkills:["client servicing","kyc","client services","international bpo","client onboarding","document verification"],
+    jobdetail:["Client Support Analyst with experience of 1 to 3 yrs","Notice: immediate to 7 days","Max CTC Offered: 3.5LPA - 4LPA","Only Delhi NCR candidates","Rotational Shifts"],
+    aboutCompany:["Quess Corp (Magna Infotech)","magna","shradha.bhatnagar@quesscorp.com"],
+    date:"Jul 17, 2021"
+},
+{
+    id:2,
+    jobTitle:"Mechanical Engineer",
+    experience:3, 
+    salary:2.5, 
+    location:"Delhi", 
+    keySkills:["gas","kyc","pumps","maintenance planing","options","project engineering","cost control","cv","mechanical system","regulatory requirements","reviews","onshore","offshore","planing","design","codes","endineering support","testing","supplier"],
+    jobdetail:["Provides technical and engineering support and guidance to offshore or on site teams"," Monitors the performance of the mechanical systems","  Ensures that mechanical systems meet statutory and regulatory requirements","Undertakes in-depth analyses/studies of problems and identifies longer-term solutions and/or options"," Undertakes general Project Engineering studies and activities"],
+    aboutCompany:["PR consultants Hiring For PR consultants","Hiring For Agent","chrisday4110@gmail.com"],
+    date:"Feb 1 2021"
+},
+{
+    id:3,
+    jobTitle:"Looking for Diploma B.tech mechanical Engineer",
+    experience:2, 
+    salary:3.0, 
+    location:"Delhi", 
+    keySkills:["Mechanical","testing","production","auto ancillary","automobile","qulity control","cost control","issues","mechanical/production","regulatory requirements"],
+    jobdetail:["Dear Sir/Mam,","We are looking for Diploma mechanical Engineer freshers","The Job description is given below.","Qualification:-Diploma mechanical Engineer freshers I.T.I all trade","Salary:- 10500 to 15000/-","Designation:- Production"],
+    aboutCompany:["ERA Consultancy","Era consultancy is providing job in only Automobile sector.","career.sony15@gmail.com"],
+    date:"Jul 23, 2021"
+},
+{
+    id:4,
+    jobTitle:"Looking for Diploma B.tech mechanical Engineer",
+    experience:2, 
+    salary:3.0, 
+    location:"Delhi", 
+    keySkills:["Mechanical","testing","production","auto ancillary","automobile","qulity control","cost control","issues","mechanical/production","regulatory requirements"],
+    jobdetail:["Dear Sir/Mam,","We are looking for Diploma mechanical Engineer freshers","The Job description is given below.","Qualification:-Diploma mechanical Engineer freshers I.T.I all trade","Salary:- 10500 to 15000/-","Designation:- Production"],
+    aboutCompany:["ERA Consultancy","Era consultancy is providing job in only Automobile sector.","career.sony15@gmail.com"],
+    date:"Mar 9, 2021"
+},
+{
+    id:5,
+    jobTitle:"Telesales / Telemarketing Executive for Loans (HL-BL-LAP)",
+    experience:4, 
+    salary:2.0, 
+    location:"Delhi", 
+    keySkills:["telesales","sales","mortgage","telecalling","mortgage sales","mortgage","lead generation"],
+    jobdetail:["Dear All ","We have an opening for the Telesales / Telemarketing Executive for Loans (HL-BL-LAP)","Location: Pusa Road, New Delhi ","CTC: Depends on candidate experience & last salary ","Salary:- 10500 to 15000/-","Designation:- Production"],
+    aboutCompany:["SMC Global Securities Ltd","SMC Group, founded in 1990, is India's leading share and stock broker"," provides a wide range of financial services and investment solutions","kanikas@smcinvestments.co.in"],
+    date:"Jul 1, 2021"
+},
+{
+    id:6,
+    jobTitle:"Corporate Finance (Debt Syndication & Project Finance) - Asst. Vice President",
+    experience:3.5, 
+    salary:3.0, 
+    location:"Delhi", 
+    keySkills:["corporate lending","secured financing","project finance","project financing","project funding","restructuring","debt syndication","term loan","corparate finance"],
+    jobdetail:["Dear Sir/Mam,","Greetings!!!","Should ensure successful completion of the assignment within Should have effective co-ordination with clients and Banks, FIs, NBFCs.","Should have excellent knowledge of preparation and review of financial.","Should have worked closely with banks, FIs, NBFCs."],
+    aboutCompany:["ERA Consultancy","Era consultancy is providing job in only Automobile sector.","career.sony15@gmail.com"],
+    date:"Mar 29, 2021"
+},
+{
+    id:7,
+    jobTitle:"Corporate Finance (Debt Syndication & Project Finance) - Asst. Vice President",
+    experience:3.5, 
+    salary:3.0, 
+    location:"Delhi", 
+    keySkills:["corporate lending","secured financing","project finance","project financing","project funding","restructuring","debt syndication","term loan","corparate finance"],
+    jobdetail:["Dear Sir/Mam,","Greetings!!!","Should ensure successful completion of the assignment within Should have effective co-ordination with clients and Banks, FIs, NBFCs.","Should have excellent knowledge of preparation and review of financial.","Should have worked closely with banks, FIs, NBFCs."],
+    aboutCompany:["ERA Consultancy","Era consultancy is providing job in only Automobile sector.","career.sony15@gmail.com"],
+    date:"Mar 29, 2021"
+},
+
+
+
+
+
+
+
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default data
