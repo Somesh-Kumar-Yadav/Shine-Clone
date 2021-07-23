d4f9d76ea4f8601779a9c871cb8ca691117a9e3b
dhruva-surya <dhruva_nj2_108@masai.school>
2021-07-23 12:11:57 +0530

    jobsearch

---
+++ src/components/showJobs/ShowJob.jsx
@@ -0,0 +1,18 @@
+import ShowNavBar from "./ShowNavBar"
+import ShowJobsPage from "./ShowJobsPage"
+
+
+export default function ShowJob(){
+
+ return <>
+       <ShowNavBar />
+       <ShowJobsPage />
+ 
+ 
+ 
+ </>
+
+
+
+
+}
