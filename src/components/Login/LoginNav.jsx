4bde062f77397c518ed0400fb4f7eacb43838237
Somesh <somesh_nj2_076@masai.school>
2021-07-25 12:28:20 +0530

    Updates

--- src/components/Login/LoginNav.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import styles from "../../styles/Login.module.css"
-export function LoginNav({home,register,login}) {
-    return <nav className={styles.log_navbar}>
-            <img onClick={()=>home()} src={process.env.PUBLIC_URL + `NavBar_logo.png`} alt="" />
-    </nav>
-}
